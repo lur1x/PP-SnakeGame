41ed4a2f5aa3093e6947529b563480cd669083fa
Kirillov Dmitrij <mr.lurix@gmail.com>
2024-10-06 16:52:13 +0300

    Сделал первую лабу

---
+++ lw1/task1/task1/task1.cpp
@@ -0,0 +1,31 @@
+﻿/*Напишите программу, которая:
+Запрашивает у пользователя строку.
+Используя методы front и back, выводит на экран первый и последний символы этой строки.
+*/
+
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string str;
+
+    std::cout << "Напишите любую строку" << std::endl;
+    getline(std::cin, str);
+
+    if (str.empty()) {
+        std::cout << "Вы ввели пустую строку" << std:: endl;
+    }
+    else {
+        std::string firstCh, lastCh;
+        firstCh = str.front();
+        lastCh = str.back();
+        std::cout << "Первый симовол строки: " << firstCh << std::endl;
+        std::cout << "Последний символ строки: " << lastCh << std::endl;
+    }
+}
---
+++ lw1/task3/task3/task3.cpp
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая:
+Запрашивает у пользователя две строки.
+Использует метод compare для сравнения этих строк.
+Выводит на экран результат сравнения (какая строка лексикографически больше, или если они равны).
+*/
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+int main() {
+
+	setlocale(LC_CTYPE, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::string str1, str2;
+	int result;
+
+	std::cout << "Введите первую строку" << std::endl;
+	getline(std::cin, str1);
+	std::cout << "Введите вторую строку" << std::endl;
+	getline(std::cin, str2);
+
+	result = str1.compare(str2);
+	std::cout << "str1.compare(str2) = " << result << std::endl;
+}
